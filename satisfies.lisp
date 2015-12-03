a097f489c97667e009cc3d07a1eebd4499091a77
James Kalenius <aeshtaer@gmail.com>
2015-12-03 14:15:15 -0800

    add satisfies

---
+++ satisfies.lisp
@@ -0,0 +1,28 @@
+(in-package #:sandalphon.types)
+
+(defclass satisfies-type (type)
+  ((predname :initarg :pred :accessor satisfies-type-predicate-name)))
+
+(defmethod typep (obj (type satisfies-type))
+  (funcall (fdefinition (satisfies-type-predicate-name type)) obj))
+
+(defmethod subtypep tri/combine
+    ((t1 satisfies-type) (t2 satisfies-type))
+  ;; someone could probably make the case that (satisfies foo) is not
+  ;;  a subtype of itself if it has side effects and can return
+  ;;  differently on the same data, but (a) then the idea of types as
+  ;;  sets is fundamentally flawed, and (b) fuck that
+  (if (eql (satisfies-type-predicate-name t1)
+	   (satisfies-type-predicate-name t2))
+      (values t t)
+      (values nil nil)))
+
+(setf (specifier-special 'satisfies nil)
+      (lambda (spec env)
+	(declare (ignore env))
+	(destructuring-bind (predicate) (rest spec)
+	  (check-type predicate symbol)
+	  (make-instance 'satisfies-type :pred predicate))))
+
+(defmethod unparse ((type satisfies-type))
+  (list 'satisfies (satisfies-type-predicate-name type)))
